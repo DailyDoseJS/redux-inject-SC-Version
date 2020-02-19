f6522bffd746cc6db62e7863ef1f2d34c54571aa
DailyDoseJS <57065316+DailyDoseJS@users.noreply.github.com>
2020-02-19 15:22:55 +0100

    Create reduxInject.bat

---
+++ reduxInject.bat
@@ -0,0 +1,37 @@
+@echo off
+
+:: BatchAdminRechte
+:-------------------------------------
+REM  --> Checke nach rechte
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> Wenn error gesetzt ist haben wir keine adminrechte.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+
+Start C:\PATH\TO\altv.exe
+
+timeout /t 20 REM--> Time in seconds where the socialclub launcher finished the patching
+
+xcopy /y "C:\PATH\TO\REDUXFOLDER" "C:\PATH\TO\Grand Theft Auto V"
+ 
